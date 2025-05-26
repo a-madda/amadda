110426358f408c59eed40ae66b0624acb8e8fec6
이승구 <leesg107@naver.com>
2025-05-27 07:44:11 +0900

    feat: 화면 개발을 위한 dependency 추가

--- chat-service/build.gradle.kts
+++ chat-service/build.gradle.kts
@@ -4,5 +4,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     /*implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")*/
+    // https://mvnrepository.com/artifact/org.webjars/bootstrap
+    implementation("org.webjars:bootstrap:5.3.5")
+    // https://mvnrepository.com/artifact/org.webjars.npm/vue
+    implementation("org.webjars.npm:vue:3.5.14")
+    // https://mvnrepository.com/artifact/org.webjars.npm/axios
+    implementation("org.webjars.npm:axios:1.9.0")
+    // https://mvnrepository.com/artifact/org.webjars/sockjs-client
+    implementation("org.webjars:sockjs-client:1.5.1")
+    // https://mvnrepository.com/artifact/org.webjars/stomp-websocket
+    implementation("org.webjars:stomp-websocket:2.3.4")
+    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
 }
