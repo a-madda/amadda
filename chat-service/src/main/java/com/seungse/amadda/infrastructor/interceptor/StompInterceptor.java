980b8ec7af8c8ef4c3ef06ce20d9a38ea2a7076a
이승구 <leesg107@naver.com>
2025-05-28 09:46:07 +0900

    feat: socket 연결시 인증 검증 로직 추가

---
+++ chat-service/src/main/java/com/seungse/amadda/infrastructor/interceptor/StompInterceptor.java
@@ -0,0 +1,26 @@
+package com.seungse.amadda.infrastructor.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StompInterceptor implements ChannelInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if(StompCommand.CONNECT == accessor.getCommand()) {
+            // TODO websocket 연결 시 인가 검사 처리 로직 추가
+        }
+        return message;
+    }
+
+}
