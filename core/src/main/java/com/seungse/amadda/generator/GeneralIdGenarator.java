af40912ed4069289354e625228e4ad94b0063700
이승구 <leesg107@naver.com>
2025-06-09 20:55:33 +0900

    feat: general id generator 추가

---
+++ core/src/main/java/com/seungse/amadda/generator/GeneralIdGenarator.java
@@ -0,0 +1,25 @@
+package com.seungse.amadda.generator;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.security.SecureRandom;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GeneralIdGenarator {
+
+    // starts at the year 2010
+    private static final long EPOCH_MILLIS = 1262304000000L;
+    // left shift amounts
+    private static final int TIMESTAMP_SHIFT = 23;
+    // exclusive
+    private static final int MAX_RANDOM = 0x800000;
+
+    private static final SecureRandom random = new SecureRandom();
+
+    public Object generate() {
+        long time = System.currentTimeMillis() - EPOCH_MILLIS;
+        return (time << TIMESTAMP_SHIFT) + random.nextInt(MAX_RANDOM);
+    }
+    
+}
