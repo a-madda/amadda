abb6fc8ba75a08765f691cc47001e2d37a07c416
노정민 <48047620+yesjm-dev@users.noreply.github.com>
2025-06-21 21:36:09 +0900

    chore: hibernate-spatial 6.6.13 버전으로 변경

--- region-service/build.gradle.kts
+++ region-service/build.gradle.kts
@@ -2,6 +2,5 @@ dependencies {
     implementation(project(mapOf("path" to ":cloud-core")))
     implementation(project(mapOf("path" to ":jpa-core")))
     runtimeOnly("org.postgresql:postgresql")
-    implementation("org.hibernate.orm:hibernate-core:6.4.4.Final")
-    implementation("org.hibernate.orm:hibernate-spatial:6.4.4.Final")
+    implementation("org.hibernate.orm:hibernate-spatial:6.6.13.Final")
 }
