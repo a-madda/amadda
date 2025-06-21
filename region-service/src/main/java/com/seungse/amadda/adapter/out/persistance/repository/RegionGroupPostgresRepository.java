f3559dd94f33c3fa5199340d373427ae38415d90
노정민 <48047620+yesjm-dev@users.noreply.github.com>
2025-06-22 01:16:24 +0900

    fix: 현재 필요없는 entityRepository 제거

--- region-service/src/main/java/com/seungse/amadda/adapter/out/persistance/repository/RegionGroupPostgresRepository.java
+++ region-service/src/main/java/com/seungse/amadda/adapter/out/persistance/repository/RegionGroupPostgresRepository.java
@@ -1,9 +1,8 @@
 package com.seungse.amadda.adapter.out.persistance.repository;
 
 import com.seungse.amadda.adapter.out.persistance.entity.RegionGroupEntity;
-import com.seungse.amadda.infrastructure.repository.RegionGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegionGroupPostgresRepository extends RegionGroupRepository<RegionGroupEntity, Long>, JpaRepository<RegionGroupEntity, Long> {
+public interface RegionGroupPostgresRepository extends JpaRepository<RegionGroupEntity, Long> {
 
 }
