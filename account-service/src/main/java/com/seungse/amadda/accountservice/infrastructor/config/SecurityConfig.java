8eba8b9b52f9eb23acef36a14f3919ddf9186cb3
cyeji <98408267+cyeji@users.noreply.github.com>
2025-06-29 21:30:17 +0900

    refactor : indent 및 람다식 변경

--- account-service/src/main/java/com/seungse/amadda/accountservice/infrastructor/config/SecurityConfig.java
+++ account-service/src/main/java/com/seungse/amadda/accountservice/infrastructor/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(c -> c.disable())
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()  // 여기선 @PreAuthorize 로 제한
-            );
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()  // 여기선 @PreAuthorize 로 제한
+                );
         return http.build();
     }
 }
