fa18f711f9bcaa6829c5283b84a59ad5490a4503
cyeji <98408267+cyeji@users.noreply.github.com>
2025-07-01 20:38:16 +0900

    feat : build.gradle 문법 변환

---
+++ account-service/build.gradle.kts
@@ -0,0 +1,24 @@
+dependencies {
+    implementation(project(mapOf("path" to ":cloud-core")))
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.auth0:java-jwt:4.5.0")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    // https://mvnrepository.com/artifact/org.keycloak/keycloak-admin-client
+    implementation("org.keycloak:keycloak-admin-client:26.0.5")
+    implementation("org.keycloak:keycloak-authz-client:17.0.1")
+
+    testImplementation("org.springframework.security:spring-security-test")
+//    runtimeOnly 'io.micrometer:micrometer-registry-prometheus'
+    annotationProcessor("org.projectlombok:lombok")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+
+}
